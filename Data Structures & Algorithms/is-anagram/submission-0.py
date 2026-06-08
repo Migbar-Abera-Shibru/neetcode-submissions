08805c4b5de13d5440155f3bbee2f46485633fb0
Migbar Abera Shibru <110468066+Migbar-Abera-Shibru@users.noreply.github.com>
2026-06-08 06:59:24 -0400

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+
+        dic ={}
+
+        for char in s:
+            dic[char]=dic.get(char,0)+1
+
+        for char in t:
+            if char not in dic:
+                return False
+
+            dic[char]-=1
+
+            if dic[char] == 0:
+                del dic[char]
+        
+        return len(dic) == 0
